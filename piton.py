16276bb59f62b6db2912904ecff941d6bde204f2
ViktorSedov60 <98118779+ViktorSedov60@users.noreply.github.com>
2022-03-23 22:36:00 +0300

    api_test_3

--- piton.py
+++ piton.py
@@ -1,31 +1,94 @@
-import turtle
+# import turtle
+#
+# turtlePen = turtle.Turtle()
+# window = turtle.Screen()
+#
+# window.bgcolor("black")
+#
+#
+# def polygon(n, size=80):
+#     if n > 4:
+#         angle = 360 / n
+#
+#         for n in range(0, n):
+#             turtlePen.color(colors[n % 5])
+#             turtlePen.left(angle)
+#             turtlePen.forward(size)
+#
+#
+# turtlePen.speed(0)
+#
+# colors = ['orange', 'cyan', 'blue', 'green', 'red']
+#
+# size = 5
+#
+# for i in range(0, 40):
+#     turtlePen.color(colors[i % 5])
+#     polygon(6, size)
+#     turtlePen.left(5)
+#     size += 3
+#
+# window.mainloop()
 
-turtlePen = turtle.Turtle()
-window = turtle.Screen()
+# class Product:
+#     def __init__(self, name, category, quantity_in_stock):
+#         self.name = name
+#         self.category = category
+#         self.quantity_in_stock = quantity_in_stock
+#
+#     def is_available(self):
+#         return True if self.quantity_in_stock > 0 else False
+#
+# eggs = Product("eggs", "food", 5)
+# brad = Product("brad", "food", 5)
+# eg = Product("eg", "food", 5)
+# fack = Product("fack", "food", 0)
+# print(fack.is_available())
 
-window.bgcolor("black")
 
 
-def polygon(n, size=80):
-    if n > 4:
-        angle = 360 / n
+#  два метода обработки объекта с классом
+# class Event:
+#     def __init__(self, timestamp, event_type, session_id):
+#         self.timestamp = timestamp
+#         self.type = event_type
+#         self.session_id = session_id
 
-        for n in range(0, n):
-            turtlePen.color(colors[n % 5])
-            turtlePen.left(angle)
-            turtlePen.forward(size)
+class Event:
+    def __init__(self, timestamp=0, event_type="", session_id=""):
+        self.timestamp = timestamp
+        self.type = event_type
+        self.session_id = session_id
 
+    def init_from_dict(self, event_dict):
+        self.timestamp = event_dict.get("timestamp")
+        self.type = event_dict.get("type")
+        self.session_id = event_dict.get("session_id")
+events = [
+    {
+     "timestamp": 1554583508000,
+     "type": "itemViewEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+    {
+     "timestamp": 1555296337000,
+     "type": "itemViewEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+    {
+     "timestamp": 1549461608000,
+     "type": "itemBuyEvent",
+     "session_id": "0:NynteeXG:MYlskrqZbcmXNSFEJaZIsNVGeDLLpmct",
+    },
+]
 
-turtlePen.speed(0)
+# for event in events:
+#     event_obj = Event(timestamp=event.get("timestamp"),
+#                       event_type=event.get("type"),
+#                       session_id=event.get("session_id"))
+#     print(event_obj.timestamp)
 
-colors = ['orange', 'cyan', 'blue', 'green', 'red']
-
-size = 10
-
-for i in range(0, 60):
-    turtlePen.color(colors[i % 5])
-    polygon(5, size)
-    turtlePen.left(5)
-    size += 5
-
-window.mainloop()
+for event in events:
+    event_obj = Event()
+    event_obj.init_from_dict(event)
+    print(event_obj.timestamp)
