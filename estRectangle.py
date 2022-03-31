815888e2a45cf21c9ce708ce4c3e6d6dc18ae282
ViktorSedov60 <98118779+ViktorSedov60@users.noreply.github.com>
2022-03-31 18:14:24 +0300

    API - 35

---
+++ estRectangle.py
@@ -0,0 +1,14 @@
+from Restangle import Rectangle
+
+r1 = Rectangle(10, 5)
+
+print("r1.width=", r1.width)
+print("r1.height=", r1.height)
+print("r1.getWigth=", r1.getWidth())
+print("r1.getArea=", r1.getArea())
+
+
+r2 = Rectangle(25,30)
+print("r2.width=", r2.width)
+print("r2.height=", r2.height)
+print("r2.getArea=", r2.getArea())
